667c73cbe86dea1fb3ee7cc6334ed326a5a54fc1
Tyoma Deev <tyoma.deev@gmail.com>
2024-06-09 13:49:25 +0200

    Restructured the app

--- api/echo.js
+++ api/echo.js
@@ -1,6 +1,27 @@
-export default function helloWorld() {
-  return new Response().json({
-    status: "success",
-    message: "Hello world",
-  });
+import { hello } from "../dep";
+import { formatRelative, subDays } from "date-fns";
+// runtime edge is a must otherwise this thing won't work at all (500)
+// if you change it to nodejs, then it will cause an error and the docs do not tell you that
+// the error message that you see in your console (or logs) misguides you by saying
+// `config.runtime: "nodejs"` semantics will evolve soon. Please remove the `runtime` key to keep the existing behavior
+// whereas runtime is mandatory if you want this to work
+export const config = {
+  runtime: "edge", // this is a pre-requisite
+};
+
+export default function GET(request) {
+  return new Response(
+    JSON.stringify({
+      status: "success",
+      message: `Three days ago, ${formatRelative(
+        subDays(new Date(), 3),
+        new Date()
+      )} `,
+    }),
+    {
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    }
+  );
 }
